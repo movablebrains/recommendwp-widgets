829458018523a5ac6bb7af6b1b2d9cbc8ecc5faa
Rotsen Mark Acob <acob.rotsen@hotmail.com>
2016-10-14 20:25:26 +0800

    Bug fixes

--- widgets/image-carousel/image-carousel.php
+++ widgets/image-carousel/image-carousel.php
@@ -15,9 +15,7 @@ class Image_Carousel_Widget extends SiteOrigin_Widget {
                 'description' => __( 'A simple image carousel widget', 'recommendwp-widgets' ),
                 'help' => ''
             ),
-            array(
-
-            ),
+            array(),
             false,
             plugin_dir_path( __FILE__ ) . 'widgets'
         );
@@ -30,104 +28,9 @@ class Image_Carousel_Widget extends SiteOrigin_Widget {
 				'label' => __('Title', 'recommendwp-widgets'),
 				'default' => ''
 			),
-			'slider_options' => array(
-				'type' => 'section',
-				'label' => __( 'Slideshow Settings', 'gss' ),
-				'hide' => true,
-				'fields' => array(
-					'class' => array(
-						'type' => 'text',
-						'label' => __( 'Class', 'gss' ),
-						'default' => ''
-					),
-					'slides' => array(
-						'type' => 'number',
-						'default' => 4,
-						'label' => __( 'Slides', 'gss' )
-					),
-					'slides_tablet' => array(
-						'type' => 'number',
-						'default' => 3,
-						'label' => __( 'Slides Tablet', 'gss' )
-					),
-					'slides_mobile' => array(
-						'type' => 'number',
-						'default' => 1,
-						'label' => __( 'Slides Tablet', 'gss' )
-					),
-					'margin' => array(
-						'type' => 'number',
-						'default' => 0,
-						'label' => __( 'Slide margin', 'gss' ),
-					),
-					'duration' => array(
-						'type' => 'number',
-						'default' => 250,
-						'label' => __( 'Duration', 'gss' ),
-					),
-					'speed' => array(
-						'type' => 'number',
-						'default' => 250,
-						'label' => __( 'Speed', 'gss' ),
-					),
-					'autoplay' => array(
-						'type' => 'checkbox',
-						'default' => false,
-						'label' => __( 'Enable autoplay?', 'gss' ),
-					),
-					'navigation' => array(
-						'type' => 'checkbox',
-						'default' => false,
-						'label' => __( 'Display navigation?', 'gss' ),
-					),
-					'pagination' => array(
-						'type' => 'checkbox',
-						'default' => false,
-						'label' => __( 'Display pagination?', 'gss' ),
-					),
-					'autoheight' => array(
-						'type' => 'checkbox',
-						'default' => false,
-						'label' => __( 'Enable autoheight?', 'gss' ),
-					),
-					'autowidth' => array(
-						'type' => 'checkbox',
-						'default' => false,
-						'label' => __( 'Enable autowidth?', 'gss' ),
-					),
-					'center' => array(
-						'type' => 'checkbox',
-						'default' => false,
-						'label' => __( 'Center item', 'gss' ),
-					),
-					'mergefit' => array(
-						'type' => 'checkbox',
-						'default' => true,
-						'label' => __( 'Fit merged items?', 'gss' ),
-					),
-					'loop' => array(
-						'type' => 'checkbox',
-						'default' => true,
-						'label' => __( 'Loop items?', 'gss' )
-					)
-				)
-			),
-			'image_settings' => array(
-				'type' => 'section',
-				'label' => __( 'Image Settings', 'gss' ),
-				'hide' => true,
-				'fields' => array(
-					'imagex' => array(
-						'type' => 'number',
-						'label' => __( 'Image Width', 'gss' ),
-						'default' => ''
-					),
-					'imagey' => array(
-						'type' => 'number',
-						'label' => __( 'Image Height(optional)', 'gss' ),
-						'default' => ''
-					),
-				)
+			'class' => array(
+				'type' => 'text',
+				'label' => __( 'Class', 'recommendwp-widgets' ),
 			),
 			'images' => array(
 				'type' => 'repeater',
@@ -157,20 +60,159 @@ class Image_Carousel_Widget extends SiteOrigin_Widget {
 						'default' => ''
 					)
 				)
+			),
+			'settings' => array(
+				'type' => 'section',
+				'label' => __( 'Image Settings', 'gss' ),
+				'hide' => true,
+				'fields' => array(
+					'imagex' => array(
+						'type' => 'number',
+						'label' => __( 'Image Width', 'gss' ),
+						'default' => ''
+					),
+					'imagey' => array(
+						'type' => 'number',
+						'label' => __( 'Image Height(optional)', 'gss' ),
+						'default' => ''
+					),
+				)
+			),
+			'slideshow' => array(
+				'type' => 'section',
+				'label' => __( 'Slideshow Settings', 'recommendwp-widgets' ),
+				'hide' => true,
+				'fields' => array(
+					'slides' => array(
+						'type' => 'number',
+						'default' => 1,
+						'label' => __( 'Slides', 'gss' )
+					),
+					'margin' => array(
+						'type' => 'number',
+						'default' => 0,
+						'label' => __( 'Slide margin', 'gss' ),
+					),
+					'duration' => array(
+						'type' => 'number',
+						'default' => 250,
+						'label' => __( 'Duration', 'gss' ),
+					),
+					'speed' => array(
+						'type' => 'number',
+						'default' => 250,
+						'label' => __( 'Speed', 'gss' ),
+					),
+					'autoplay' => array(
+						'type' => 'checkbox',
+						'default' => false,
+						'label' => __( 'Enable autoplay?', 'gss' ),
+					),
+					'navigation' => array(
+						'type' => 'checkbox',
+						'default' => true,
+						'label' => __( 'Display navigation?', 'gss' ),
+					),
+					'pagination' => array(
+						'type' => 'checkbox',
+						'default' => false,
+						'label' => __( 'Display pagination?', 'gss' ),
+					),
+					'autoheight' => array(
+						'type' => 'checkbox',
+						'default' => false,
+						'label' => __( 'Enable autoheight?', 'gss' ),
+					),
+					'autowidth' => array(
+						'type' => 'checkbox',
+						'default' => false,
+						'label' => __( 'Enable autowidth?', 'gss' ),
+					),
+					'center' => array(
+						'type' => 'checkbox',
+						'default' => false,
+						'label' => __( 'Center item', 'gss' ),
+					),
+					'mergefit' => array(
+						'type' => 'checkbox',
+						'default' => false,
+						'label' => __( 'Fit merged items?', 'gss' ),
+					),
+					'loop' => array(
+						'type' => 'checkbox',
+						'default' => true,
+						'label' => __( 'Loop items?', 'gss' )
+					)
+				)
+			),
+			'responsive' => array(
+				'type' => 'section',
+				'label' => __( 'Responsive Settings', 'recommendwp-widgets' ),
+				'hide' => true,
+				'fields' => array(
+					'mobile' => array(
+						'type' => 'number',
+						'label' => __( 'Slides for mobile', 'recommendwp-widgets' ),
+						'default' => 1
+					),
+					'tablet' => array(
+						'type' => 'number',
+						'label' => __( 'Slides for tablets', 'recommendwp-widgets' ),
+						'default' => 1
+					),
+				)
+			),
+			'template' => array(
+				'type' => 'select',
+				'label' => __( 'Choose template', 'recommendwp-widgets' ),
+				'options' => array(
+					'default' => __( 'Default', 'recommendwp-widgets' )
+				),
+				'default' => 'default'
 			)
 		);
 	}
 
     function get_template_name( $instance ) {
-        return 'default';
+        switch ( $instance['template'] ) {
+            case 'default':
+            default:
+                return 'default';
+                break;
+        }
     }
 
     function get_style_name( $instance ) {
-        return '';
+        switch ( $instance['template'] ) {
+            case 'default':
+            default:
+                return 'default';
+                break;
+        }
     }
 
 	function get_template_variables( $instance, $args ) {
-
+		return array(
+    		'title' => $instance['title'],
+    		'class' => $instance['class'],
+    		'width' => $instance['settings']['imagex'],
+    		'height' => $instance['settings']['imagey'],
+    		'template' => $instance['template'],
+			'slides' => $instance['slideshow']['slides'],
+			'margin' => $instance['slideshow']['margin'],
+			'duration' => $instance['slideshow']['duration'],
+			'speed' => $instance['slideshow']['speed'],
+			'autoplay' => $instance['slideshow']['autoplay'],
+			'navigation' => $instance['slideshow']['navigation'],
+			'pagination' => $instance['slideshow']['pagination'],
+			'autowidth' => $instance['slideshow']['autowidth'],
+			'autoheight' => $instance['slideshow']['autoheight'],
+			'center' => $instance['slideshow']['center'],
+			'mergefit' => $instance['slideshow']['mergefit'],
+			'loop' => $instance['slideshow']['loop'],
+			'slides_mobile' => $instance['responsive']['mobile'],
+			'slides_tablet' => $instance['responsive']['tablet']
+    	);
 	}
 }
 
