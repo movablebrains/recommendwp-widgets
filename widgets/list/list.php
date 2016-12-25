1cb8064435e1466ff309f1e7054028a5e44ffead
Rotsen Mark Acob <acob.rotsen@hotmail.com>
2016-12-26 06:55:36 +0800

    Added List Widget

---
+++ widgets/list/list.php
@@ -0,0 +1,128 @@
+<?php
+/*
+Widget Name: RWP Lists
+Description: A simple list widget.
+Author: RecommendWP
+Author URI: http://www.recommendwp.com
+*/
+
+class List_Widget extends SiteOrigin_Widget {
+    function __construct() {
+        parent::__construct(
+            'rwpw-lists',
+            __( 'RWP Lists', 'recommendwp-widgets' ),
+            array(
+                'description' => __( 'A simple list widget', 'recommendwp-widgets' ),
+                'help' => ''
+            ),
+            array(),
+            false,
+            plugin_dir_path( __FILE__ ) . 'widgets'
+        );
+    }
+
+	function get_widget_form() {
+		return array(
+			'title' => array(
+				'type' => 'text',
+				'label' => __('Title', 'recommendwp-widgets'),
+				'default' => ''
+			),
+			'class' => array(
+				'type' => 'text',
+				'label' => __( 'Class', 'recommendwp-widgets' ),
+			),
+			'lists' => array(
+				'type' => 'repeater',
+				'label' => __('Add List', 'recommendwp-widgets'),
+				'item_name' => __('List', 'recommendwp-widgets'),
+				'item_label' => array(
+					'selector' => "[id*='repeat_text']",
+					'update_event' => 'change',
+					'value_method' => 'val'
+				),
+				'fields' => array(
+					'title' => array(
+						'type' => 'text',
+						'label' => __( 'List Title', 'recommendwp-widgets' ),
+						'default' => ''
+					),
+					'content' => array(
+						'type' => 'textarea',
+						'label' => __('List Content', 'recommendwp-widgets'),
+						'default' => ''
+					)
+				)
+			),
+			'icon' => array(
+				'type' => 'section',
+				'label' => __( 'Icon Settings', 'gss' ),
+				'hide' => true,
+				'fields' => array(
+                    'image' => array(
+                        'type' => 'media',
+                        'label' => __('Choose an image', 'recommendwp-widgets'),
+                        'choose' => __('Choose image', 'recommendwp-widgets'),
+                        'update' => __('Set image', 'recommendwp-widgets'),
+                        'library' => 'image'
+                    ),
+                    'size' => array(
+        				'type' => 'number',
+        				'label' => __( 'Icon size', 'recommendwp-widgets' ),
+        				'default' => '16'
+        			),
+					'color' => array(
+						'type' => 'color',
+						'label' => __( 'Color', 'recommendwp-widgets' ),
+						'default' => '#fff'
+					),
+					'hover' => array(
+						'type' => 'color',
+						'label' => __( 'Color hover', 'recommendwp-widgets' ),
+						'default' => '#fff'
+					),
+				)
+			),
+            'template' => array(
+				'type' => 'select',
+				'label' => __( 'Choose template', 'recommendwp-widgets' ),
+				'options' => array(
+					'default' => __( 'Default', 'recommendwp-widgets' )
+				),
+				'default' => 'default'
+			)
+		);
+	}
+
+    function get_template_name( $instance ) {
+        switch ( $instance['template'] ) {
+            case 'default':
+            default:
+                return 'default';
+                break;
+        }
+    }
+
+    function get_style_name( $instance ) {
+        switch ( $instance['template'] ) {
+            case 'default':
+            default:
+                return 'default';
+                break;
+        }
+    }
+
+	function get_template_variables( $instance, $args ) {
+		return array(
+    		'title' => $instance['title'],
+    		'class' => $instance['class'],
+    		'icon_image' => $instance['icon']['image'],
+            'icon_size' => $instance['icon']['size'],
+            'icon_color' => $instance['icon']['color'],
+            'icon_hover' => $instance['icon']['hover'],
+            'template' => $instance['template']
+    	);
+	}
+}
+
+siteorigin_widget_register( 'rwpw-lists', __FILE__, 'List_Widget' );
---
+++ widgets/list/tpl/default.php
@@ -0,0 +1,49 @@
+<?php
+$title = $instance['title'];
+
+if ( $title ) {
+    echo $args['before_title'] . apply_filters( 'widget_title', $title ) . $args['after_title'];
+}
+
+$widget_id = $args['widget_id'];
+
+$widget_id = preg_replace( '/[^0-9]/', '', $widget_id );
+
+echo '<div class="list-widget">';
+    $lists = $instance['lists'];
+
+    $classes = array();
+
+    $classes[] = 'rwpw-list';
+    $classes[] = $instance['class'];
+
+    $attr = array(
+        'id' => 'list-' . (int)$widget_id,
+        'class' => esc_attr( implode( ' ', $classes ) ),
+        'data-instance' => (int)$widget_id
+    );
+
+    if ( is_array( $lists ) && !is_wp_error( $lists ) ) { ?>
+        <ul <?php foreach( $attr as $name => $value ) echo $name . '="' . $value . '" ' ?>>
+            <?php foreach( $lists as $list ) { ?>
+                <li>
+                    <div class="list-wrapper">
+                        <?php if( !empty( $icon_image ) ) {
+                            $attachment = wp_get_attachment_image_src( $icon_image, 'full' );
+                            $image = rwpw_thumb( $attachment[0], $width, $height, true );
+                            echo '<div class="list-image"><img src="'.$image.'" alt="Icon" /></div>';
+                        } ?>
+                        <div class="list-content">
+                            <?php 
+                            $title = $list['title'];
+                            $content = $list['content'];
+                            echo $title ? '<h4 class="list-title">'.$title.'</h4>' : '';
+                            echo $content ? wpautop( $content, true ) : '';
+                            ?>
+                        </div>
+                    </div>
+                </li>
+            <?php } ?>
+        </ul>
+    <?php }
+echo '</div>';
