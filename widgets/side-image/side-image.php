519ee62fe42e1c9e1be7d73d5f2920c7cf3fa6b2
Rotsen Mark Acob <acob.rotsen@hotmail.com>
2016-12-26 14:41:10 +0800

    Bug fixes

--- widgets/side-image/side-image.php
+++ widgets/side-image/side-image.php
@@ -15,37 +15,56 @@ class RWP_Side_Image extends SiteOrigin_Widget {
 				'description' => __( 'A simple widget based of Bourbon Refills Side Image', 'recommendwp-widgets' ),
 				'help' => ''
 			),
-			array(
-
-			),
-			array(
-				'image' => array(
-					'type' => 'media',
-					'label' => __('Choose an image', 'recommendwp-widgets'),
-					'choose' => __('Choose image', 'recommendwp-widgets'),
-					'update' => __('Set image', 'recommendwp-widgets'),
-					'library' => 'image'
-				),
-				'editor' => array(
-					'type' => 'widget',
-					'label' => __( 'Editor', 'recommendwp-widgets' ),
-					'class' => 'SiteOrigin_Widget_Editor_Widget',
-					'hide' => false
-				)
-			),
-
+			array(),
+			false,
 			plugin_dir_path( __FILE__ ) . 'widgets'
 		);
 	}
 
+	function initialize() {
+
+	}
+
+	function get_widget_form() {
+		return array(
+			'image' => array(
+				'type' => 'media',
+				'label' => __('Choose an image', 'recommendwp-widgets'),
+				'choose' => __('Choose image', 'recommendwp-widgets'),
+				'update' => __('Set image', 'recommendwp-widgets'),
+				'library' => 'image'
+			),
+			'editor' => array(
+				'type' => 'widget',
+				'label' => __( 'Editor', 'recommendwp-widgets' ),
+				'class' => 'SiteOrigin_Widget_Editor_Widget',
+				'hide' => false
+			),
+			'template' => array(
+				'type' => 'select',
+				'label' => __( 'Template', 'recommendwp-widgets' ),
+				'options' => array(
+					'default' => 'Default',
+				),
+				'default' => 'default'
+			)
+		);
+	}
+
 	function get_template_name( $instance ) {
-		return 'default';
+		switch ( $instance['template'] ) {
+            case 'default':
+            default:
+                return 'default';
+                break;
+        }
 	}
 
 	function get_template_variables( $instance, $args ) {
 		return array(
 			'image' => $instance['image'],
-			'size' => 'full'
+			'size' => 'full',
+			'template' => $instance['template']
 		);
 	}
 }
