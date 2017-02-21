843c3db4d1e301df4f753319b381eac4adf8fd37
Rotsen Mark Acob <acob.rotsen@hotmail.com>
2017-02-22 07:32:33 +0800

    Change textarea to tinymce

--- widgets/button/button.php
+++ widgets/button/button.php
@@ -164,8 +164,18 @@ class RWP_Button_Widget extends SiteOrigin_Widget {
                         )
                     ),
                     'content' => array(
-                        'type' => 'textarea',
-                        'label' => __( 'Popup Content', 'recommendwp-widgets' )
+                        'type' => 'tinymce',
+                        'label' => __( 'Popup Content', 'recommendwp-widgets' ),
+                        'default' => '',
+                        'rows' => 10,
+                        'default_editor' => 'tinymce',
+                        'button_filters' => array(
+                            'mce_buttons' => array( $this, 'filter_mce_buttons' ),
+                            'mce_buttons_2' => array( $this, 'filter_mce_buttons_2' ),
+                            'mce_buttons_3' => array( $this, 'filter_mce_buttons_3' ),
+                            'mce_buttons_4' => array( $this, 'filter_mce_buttons_5' ),
+                            'quicktags_settings' => array( $this, 'filter_quicktags_settings' ),
+                        ),
                     )
                 ) 
             ),
