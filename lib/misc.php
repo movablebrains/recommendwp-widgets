b79c9d33e06c90f7054a8259358044fa3958657e
Rotsen Mark Acob <acob.rotsen@hotmail.com>
2017-04-06 13:49:57 +0800

    Added custom image function

--- lib/misc.php
+++ lib/misc.php
@@ -2,3 +2,27 @@
 function rwpw_thumb($url, $width, $height=0, $align='') {
     return mr_image_resize($url, $width, $height, true, $align, false);
 }
+
+// Custom Image Function
+function rwpw_post_image() {
+	global $post;
+	$image = '';
+	$image_id = get_post_thumbnail_id( $post->ID );
+	$image = wp_get_attachment_image_src( $image_id, 'full' );
+	$image = $image[0];
+	if ( $image ) return $image;
+	return rwpw_get_first_image();
+}
+
+// Get the First Image Attachment Function
+function rwpw_get_first_image() {
+	global $post, $posts;
+	$first_img = '';
+	ob_start();
+	ob_end_clean();
+	$output = preg_match_all( '/<img.+src=[\'"]([^\'"]+)[\'"].*>/i', $post->post_content, $matches );
+	$first_img = "";
+	if ( isset( $matches[1][0] ) )
+		$first_img = $matches[1][0];
+	return $first_img;
+}
