42924a8c4bee458fdbc34e08bb8ef2e79028d40f
Rotsen Mark Acob <acob.rotsen@hotmail.com>
2017-06-02 11:00:22 +0800

    Added new side-image template

---
+++ widgets/side-image/tpl/image.php
@@ -0,0 +1,25 @@
+<?php
+$imagesource = wp_get_attachment_image_src( $image, $size );
+$url = esc_url( $imagesource[0] );
+$classes = array();
+$classes[] = 'side-image';
+$classes[] = $image_position;
+$classes[] = 'side-image-' . $template;
+?>
+<div class="<?php echo esc_attr( implode( ' ', $classes ) ); ?>">
+	<?php if ( $display_image == true ) { ?>
+	<div class="image-wrapper">
+        <img src="<?php echo $url; ?>" alt="" />
+	</div>
+	<?php } ?>
+	<div class="content-wrapper">
+		<?php if ( $display_content == true ) { ?>
+			<?php $this->sub_widget('SiteOrigin_Widget_Editor_Widget', $args, $instance['editor']) ?>
+		<?php } ?>
+		<?php if ( $display_button == true ) { ?>
+			<div class="side-image-button">
+				<?php $this->sub_widget( 'RWP_Button_Widget', $args, $instance['button'] ); ?>
+			</div>
+		<?php } ?>
+	</div>
+</div>
